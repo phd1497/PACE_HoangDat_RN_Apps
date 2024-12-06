08599e48124a58a0658996e3a474f6ea08fc7dfe
phd1497 <72797290+phd1497@users.noreply.github.com>
2024-12-06 14:03:15 -0600

    Test Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.header}>
+        <Text style={styles.title}>My Bio</Text>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Full Name:</Text>
+        <Text style={styles.sectionContent}>Hoang Dat Phan</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flexGrow: 1,
+    padding: 20,
+    backgroundColor: '#f5f5f5',
+  },
+  header: {
+    marginBottom: 20,
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
